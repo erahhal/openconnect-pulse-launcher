944f5de26c1e3cea6dcb24fe06e926c834078a0c
Ellis Rahhal <erahhal@netflix.com>
2025-02-04 11:50:33 -0800

    fixed nixpkgs version to avoid breakages

--- flake.nix
+++ flake.nix
@@ -1,6 +1,10 @@
 {
   description = "Script for connecting to PulseVPN using openconnect and chromedriver";
 
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11";
+  };
+
   outputs = { self, nixpkgs }: {
     packages.x86_64-linux = let
       pkgs = import "${nixpkgs}" {
